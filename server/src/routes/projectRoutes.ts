ff03c8f38cce6d72ac4e13da34322a893b64cf41
Coodos <info@coodos.co>
2021-08-06 15:50:25 +0530

    fixed path

--- server/src/routes/projectRoutes.ts
+++ server/src/routes/projectRoutes.ts
@@ -14,7 +14,10 @@ import { ensureAuthorized, ensureIsAdmin } from '../middleware/auth';
 const router = express.Router();
 
 router.route('/').post(ensureAuthorized, createProject).get(indexProjects);
-router.route('/:id').get(getProjectById).put(ensureAuthorized, editProject);
+router
+  .route('/by-id/:id')
+  .get(getProjectById)
+  .put(ensureAuthorized, editProject);
 router.route('/:id/toggle-upvote').get(ensureAuthorized, toggleProjectLike);
 router.route('/:id/add-backed').get(ensureAuthorized, addBackedProject);
 router.route('/trending').get(trendingProjects);
